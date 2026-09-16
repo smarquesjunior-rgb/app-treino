c0690bd9e11bcce4aa15e242f4cd4c533d061f9d
smarquesjunior-rgb <smarquesjunior@gmail.com>
2026-09-16 13:06:17 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('treino-v1').then((cache) => cache.addAll(['./', './index.html'])));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
